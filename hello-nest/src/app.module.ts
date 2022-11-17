0be0354e50ed8e08aa1684c48f7fb743ef252d1f
Mac_yj <pknb213@naver.com>
2022-11-17 21:30:10 +0900

    update

--- hello-nest/src/app.module.ts
+++ hello-nest/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AuthService} from "./auth/auth.service";
+import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService, UsersService],
 })
 export class AppModule {}
