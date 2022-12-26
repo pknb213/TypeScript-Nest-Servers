a7fc575b576cd60e16784981714b9d0af9f989b0
CheonYoungJo <pknb213@naver.com>
2022-12-26 23:50:29 +0900

    Updated Mock

--- hello-nest/src/movies/movies.controller.spec.ts
+++ hello-nest/src/movies/movies.controller.spec.ts
@@ -15,4 +15,19 @@ describe('MoviesController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  const mockFn = jset.fn()
+
+  const boundMockFn = mockFn.bind(thisContext0);
+  boundMockFn('a', 'b');
+  mockFn.call(thisContext1, 'a', 'b');
+  mockFn.apply(thisContext2, ['a', 'b']);
+
+  mockFn.mock.contexts[0] === thisContext0; // true
+  mockFn.mock.contexts[1] === thisContext1; // true
+  mockFn.mock.contexts[2] === thisContext2; // true
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
 });
