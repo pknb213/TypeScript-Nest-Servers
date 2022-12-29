acc8468beecff4ddb1499da75b24dec1e96a13d2
youngjo.cheon <youngjo.cheon@balaan.co.kr>
2022-12-29 18:38:37 +0900

    updaded

--- hello-nest/src/app.module.ts
+++ hello-nest/src/app.module.ts
@@ -7,8 +7,10 @@ import { UsersModule } from './users/users.module';
 import { UsersService } from './users/users.service';
 import {JwtModule, JwtService} from "@nestjs/jwt";
 import { MoviesModule } from './movies/movies.module';
+import { MovieTypesModule } from './movie-types/movie-types.module';
+import { MsaTestModule } from './msa_test/msa_test.module';
 @Module({
-  imports: [AuthModule, UsersModule, JwtModule, MoviesModule],
+  imports: [AuthModule, UsersModule, JwtModule, MoviesModule, MovieTypesModule, MsaTestModule],
   controllers: [AppController],
   providers: [AppService],
 })
