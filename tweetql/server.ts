a25385bbcf78dde473b7210bb7694ad119650967
CheonYoungJo <pknb213@naver.com>
2023-01-13 01:08:34 +0900

    feat: Updated GraphQL

--- tweetql/server.ts
+++ tweetql/server.ts
@@ -1,33 +1,35 @@
 import {ApolloServer} from "@apollo/server"
 import {startStandaloneServer} from "@apollo/server/standalone";
+import {randomInt} from "crypto";
 
 const typeDefs = `#graphql
 # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-# This "Book" type defines the queryable fields for every book in our data source.
-type User {
-    id: ID
-    username: String
-}
-
-# Fucking
-type Tweet {
-    id: ID
-    text: String
-    author: User
-
-}
-# Query~
-type Query {
-    allTweets: [Tweet]
-    tweet(id: ID): Tweet
-}
-# Mutation~
-type Mutation{
-    postTweet(text: String, userId: ID): Tweet
-    deleteTweet(id:ID): Boolean
-}
+    # This "Book" type defines the queryable fields for every book in our data source.
+    type User {
+        id: ID!
+        username: String
+    }
+    
+    # Fucking
+    type Tweet {
+        id: ID!
+        text: String
+        author: User
+    }
+    # Query~
+    type Query {
+        allTweets: [Tweet!]!
+        tweet(id: ID!): Tweet
+        ping: String!
+    }
+    # Mutation~
+    type Mutation{
+        postTweet(text: String, userId: String): Tweet
+        deleteTweet(id:ID): Boolean
+    }
 `;
+
 const tweets = [
     {
         id: '1',
@@ -49,7 +51,29 @@ const tweets = [
 const resolvers = {
     Query: {
         allTweets: () => tweets,
+        tweet() {
+            console.log("I'm Tweet Called~")
+            return null
+        },
+        ping() {
+            return "pong"
+        }
     },
+    Mutation: {
+        postTweet: (text:string, userId:string): Object | undefined | null | void => {
+            console.log("Post Tweet")
+            const newTweet = {
+                id: (tweets.length + 1).toString(),
+                text: text,
+                author: {
+                    id: +userId,
+                    username: randomInt(0, 100).toString()
+                }
+            }
+            tweets.push(newTweet);
+            return newTweet
+        }
+    }
 };
 
 const server = new ApolloServer({
