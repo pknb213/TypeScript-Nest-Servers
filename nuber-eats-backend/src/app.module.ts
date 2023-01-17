fcd6ccf96e7fe8775841f11d958c6d0468d3d81c
cyj <pknb213@naver.com>
2023-01-18 03:04:29 +0900

    Feature: Config, Valid

--- nuber-eats-backend/src/app.module.ts
+++ nuber-eats-backend/src/app.module.ts
@@ -3,22 +3,38 @@ import {GraphQLModule} from "@nestjs/graphql";
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import { RestaurantModule } from './restaurant/restaurant.module';
+import {ConfigModule} from "@nestjs/config";
+import * as Joi from 'joi'
+
 
 @Module({
   imports: [
-      GraphQLModule.forRoot({
-          driver: ApolloDriver,
-          autoSchemaFile: true,
+      ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+          ignoreEnvFile: process.env.NODE_ENV === 'prod',
+          validationSchema: Joi.object({
+              NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+              DB_HOST: Joi.string().required(),
+              DB_PORT: Joi.string().required(),
+              DB_USERNAME: Joi.string().required(),
+              DB_PASSWORD: Joi.string().required(),
+              DB_NAME: Joi.string().required()
+          })
       }),
       TypeOrmModule.forRoot({
           type: 'postgres',
-          host: '127.0.0.1',
-          port: 5432,
-          username: 'developer',
-          password: 'devpassword',
-          database: 'nuber-eats',
+          host: process.env.DB_HOST,
+          port: +process.env.DB_PORT,
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
           synchronize: true,
           logging: true,
+      }),
+      GraphQLModule.forRoot({
+          driver: ApolloDriver,
+          autoSchemaFile: true,
       }),
       RestaurantModule
   ],
